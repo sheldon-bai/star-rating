3f089f128da6af32cc22f27917a1c978d14bbb38
sheldon-bai <sheldon.bai1989@gmail.com>
2024-09-14 17:14:27 -0500

    start rating

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+import Star from './Star.vue'
+
+const stars = ref([1, 2, 3, 4, 5])
+const selectedStar = ref(0)
+const rating = ref(0)
+
+function setRating(star) {
+  rating.value = star
+}
+
+function setHoverRating(star) {
+  selectedStar.value = star
+}
+</script>
+
+<template>
+  <span v-for="star in stars">
+    <Star
+      :id="star"
+      :selected-star="selectedStar || rating"
+      @click="setRating(star)"
+      @mouseenter="setHoverRating(star)"
+      @mouseleave="setHoverRating(0)"
+    ></Star>
+  </span>
+</template>
---
+++ src/Star.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'Star',
+  computed: {
+    fillClass() {
+      if (this.id <= this.selectedStar) {
+        return 'filled'
+      }
+
+      return 'blank'
+    }
+  },
+  props: {
+    id: {
+      type: Number
+    },
+    selectedStar: {
+      type: Number,
+    }
+  },
+}
+</script>
+
+<template>
+  <span
+    :class="fillClass"
+  >*</span>
+</template>
+
+<style>
+  .filled {
+    color: red;
+  }
+
+  .blank {
+    color: orange;
+  }
+</style>
